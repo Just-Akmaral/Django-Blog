afbda70b14d6e8cc735ebaf4145f7cc87c4d282b
Just-Student <rei.kasano@gmail.com>
2017-11-11 04:27:29 +0300

    add:Add likes

--- article/urls.py
+++ article/urls.py
@@ -5,4 +5,5 @@ app_name = 'article'
 urlpatterns = [
     url(r'^$', views.ArticlesView.as_view(), name='articles'),
     url(r'^(?P<pk>[0-9]+)/$', views.ArticleView.as_view(), name='article'),
+    url(r'^addLike/(?P<article_id>[0-9]+)/$', views.addLike, name='addLike'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
 
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -12,25 +14,17 @@ class ArticlesView(generic.ListView):
     context_object_name = 'articles'
     template_name = 'article/articles.html'
 
-
     def get_queryset(self):
         articles = Article.objects.all()
-        # Отбираем первые 10 статей
-        paginator = Paginator(articles, 5)
+        paginator = Paginator(articles, 3)
         page = self.request.GET.get('page')
         try:
             articles = paginator.page(page)
         except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
             articles = paginator.page(1)
         except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
             articles = paginator.page(paginator.num_pages)
         return articles
-
-    # def get_queryset(self):
-    #     return Article.objects.filter(article_date__lte=timezone.now()).order_by('-article_date')[:3]
-
 
     def __unicode__(self):
         return self.title
@@ -50,3 +44,14 @@ class ArticleView(generic.DetailView):
         context = super(ArticleView, self).get_context_data(**kwargs)
         context['comments_list'] = Comments.objects.filter(comments_article_id=self.object)
         return context
+
+
+def addLike(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    try:
+    #    article = Article.objects.get(pk=article_id)
+        article.article_likes += 1
+        article.save()
+    except ObjectDoesNotExist:
+        raise Http404
+    return HttpResponseRedirect(reverse('article:article', args=(article.id,)))
